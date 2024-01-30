f64a5be47fa43091fbdf095df3a62e9abf7728e8
Rajkumar Kumaravelu <kumaravelu.r@northeastern.edu>
2024-01-30 13:52:12 -0500

    Initial commit with all folders and files

---
+++ service-registry/src/test/java/com/quizApp/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizApp.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
