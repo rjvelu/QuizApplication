f925f8e227a7634ca63c9c9b8d1d8620331dfe55
Rajkumar Kumaravelu <kumaravelu.r@northeastern.edu>
2024-01-30 13:52:12 -0500

    Initial commit with all folders and files

---
+++ quiz-service/src/main/java/com/quizApp/quizservice/model/QuizDTO.java
@@ -0,0 +1,11 @@
+package com.quizApp.quizservice.model;
+
+import lombok.Data;
+
+@Data
+public class QuizDTO {
+        String categoryName;
+        Integer numQuestions;
+        String title;
+}
+
